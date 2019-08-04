6f7c4f2a86aef8297234a9f8136a35c0621e2140
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-08-04 20:47:02 +0300

    Arttırma-Azaltma Operatorleri eklendi.

---
+++ src/ArttırmaveAzaltmaOperator/ArttirmaAzaltmaOperatoru.kt
@@ -0,0 +1,15 @@
+package ArttırmaveAzaltmaOperator
+
+fun main(args: Array<String>) {
+
+    var number1 = 8
+    var number2 = 3
+
+    // arttırma değeri - number1 = number1 + 1
+    number1++
+    println(number1)
+
+    // azaltma değeri - number1 = number1 - 1
+    number2--
+    println(number2)
+}
---
+++ src/MantıksalOperator/NotOperator.kt
@@ -0,0 +1,7 @@
+package MantıksalOperator
+
+fun main(args: Array<String>) {
+
+    println(!false)
+    println(!true)
+}
