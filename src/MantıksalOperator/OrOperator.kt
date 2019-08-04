c8502386e783b27ce72d91285ed17eff0a718251
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-08-04 20:03:06 +0300

    OR Operator eklendi.

---
+++ src/MantıksalOperator/OrOperator.kt
@@ -0,0 +1,16 @@
+package MantıksalOperator
+
+fun main(args: Array<String>) {
+
+    println(false || false)
+    println(false || true)
+    println(true || false)
+    println(true || true)
+
+    // --------------------
+
+    val num1 : Int = 4
+    val num2 : Int = 10
+
+    println(num1 < 3 || num2 > 9)
+}
