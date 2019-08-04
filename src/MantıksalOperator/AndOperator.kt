ad31aa9ca1bc935c9ff82b28b6e3a3183790e563
halilozel1903@gmail.com <halilozel1903@gmail.com>
2019-08-04 18:37:53 +0300

    And Operator eklendi.

---
+++ src/MantıksalOperator/AndOperator.kt
@@ -0,0 +1,16 @@
+package MantıksalOperator
+
+fun main(args: Array<String>) {
+
+    println(false && false)
+    println(false && true)
+    println(true && false)
+    println(true && true)
+
+    // --------------------
+
+    val num1 : Int = 12
+    val num2 : Int = 9
+
+    println(num1 == 12 && num2 == 9)
+}
